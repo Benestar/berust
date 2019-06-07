0d408810b5316e0ddbf1a45a44d9889241bc988b
Benedikt Seidl <benedikt.seidl@gmail.com>
2019-06-07 17:26:57 +0200

    Move main.rs to bin/befunge.rs

--- src/bin/befunge.rs
+++ src/bin/befunge.rs
@@ -11,7 +11,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        println!("Usage: ./berust <file>");
+        println!("Usage: ./befunge <file>");
 
         process::exit(1);
     }
